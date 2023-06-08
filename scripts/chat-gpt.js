49b6386b5cf510286d32e4d2ef23459b38867d95
Henrik Röschmann <henrik.roschmann@gmail.com>
2023-06-08 18:44:21 +0200

    linting

--- scripts/chat-gpt.js
+++ scripts/chat-gpt.js
@@ -1,71 +1,71 @@
 import { moduleName, getGamePromptSetting } from './settings.js';
 
-async function callGptApi(query) {    
-    const apiKey = game.settings.get(moduleName, 'openAIKey');
-    const model = game.settings.get(moduleName, 'modelVersion');
-    const prompt = getGamePromptSetting();
-    const apiUrl = 'https://api.openai.com/v1/chat/completions';
-    const promptMessage = { role: 'system', content: prompt };
-    const queryMessage = { role: 'user', content: query };
-    const messages = [promptMessage, queryMessage]
+async function callGptApi(query) {
+	const apiKey = game.settings.get(moduleName, 'openAIKey');
+	const model = game.settings.get(moduleName, 'modelVersion');
+	const prompt = getGamePromptSetting();
+	const apiUrl = 'https://api.openai.com/v1/chat/completions';
+	const promptMessage = { role: 'system', content: prompt };
+	const queryMessage = { role: 'user', content: query };
+	const messages = [promptMessage, queryMessage]
 
-    const requestBody = {
-        model,
-        messages,
-        temperature: 0.1,
-    };
+	const requestBody = {
+		model,
+		messages,
+		temperature: 0.1,
+	};
 
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify(requestBody),
-    };
+	const requestOptions = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${apiKey}`,
+		},
+		body: JSON.stringify(requestBody),
+	};
 
-    const is4xx = c => c >= 400 && c < 500;
-    const handleFailedQuery = async (response, msg) => {
-        let err = `${response?.status}`;
-        try {
-            const data = await response.json();
-            console.debug(`${moduleName} | callGptApi(): failure data =`, data);
-            err = `${data?.error?.message} (${err})`;
-        } catch (e) {
-            console.warn(`${moduleName} | Could not decode failed API response.`, e);
-        }
-        throw new Error(`${msg}: ${err}`);
-    };
+	const is4xx = c => c >= 400 && c < 500;
+	const handleFailedQuery = async (response, msg) => {
+		let err = `${response?.status}`;
+		try {
+			const data = await response.json();
+			console.debug(`${moduleName} | callGptApi(): failure data =`, data);
+			err = `${data?.error?.message} (${err})`;
+		} catch (e) {
+			console.warn(`${moduleName} | Could not decode failed API response.`, e);
+		}
+		throw new Error(`${msg}: ${err}`);
+	};
 
-    let response = {};
-    for (
-        let retries = 0, backoffTime = 5000;
-        retries < 5 && !response?.ok;
-        retries++, await new Promise(r => setTimeout(r, backoffTime))
-    ) {
-        console.debug(`${moduleName} | callGptApi(): waiting for reply (tries: ${retries})`);
-        response = await fetch(apiUrl, requestOptions);
-        console.debug(`${moduleName} | callGptApi(): response =`, response);
-        if (response?.status && is4xx(response?.status)) {
-            await handleFailedQuery(response, "ChatGPT API failed");
-        }
-    }
+	let response = {};
+	for (
+		let retries = 0, backoffTime = 5000;
+		retries < 5 && !response?.ok;
+		retries++, await new Promise(r => setTimeout(r, backoffTime))
+	) {
+		console.debug(`${moduleName} | callGptApi(): waiting for reply (tries: ${retries})`);
+		response = await fetch(apiUrl, requestOptions);
+		console.debug(`${moduleName} | callGptApi(): response =`, response);
+		if (response?.status && is4xx(response?.status)) {
+			await handleFailedQuery(response, "ChatGPT API failed");
+		}
+	}
 
-    if (response?.ok) {
-        const data = await response.json();
-        console.debug(`${moduleName} | callGptApi(): response data =`, data);
+	if (response?.ok) {
+		const data = await response.json();
+		console.debug(`${moduleName} | callGptApi(): response data =`, data);
 
-        const replyMessage = data.choices[0].message;
-        return replyMessage.content.trim();
-    } else {
-        await handleFailedQuery(response, "ChatGPT API failed multiple times");
-    }
+		const replyMessage = data.choices[0].message;
+		return replyMessage.content.trim();
+	} else {
+		await handleFailedQuery(response, "ChatGPT API failed multiple times");
+	}
 }
 
 export async function getGptReplyAsHtml(query) {
-    console.log("hej hej hapå ", query)
-    const answer = await callGptApi(query);
-    const html = /<\/?[a-z][\s\S]*>/i.test(answer) || !answer.includes('\n') ?
-        answer : answer.replace(/\n/g, "<br>");
-    return html.replaceAll("```", "");
+	console.log("hej hej hapå ", query)
+	const answer = await callGptApi(query);
+	const html = /<\/?[a-z][\s\S]*>/i.test(answer) || !answer.includes('\n') ?
+		answer : answer.replace(/\n/g, "<br>");
+	return html.replaceAll("```", "");
 }
--- scripts/npcHandler.js
+++ scripts/npcHandler.js
@@ -1,65 +1,65 @@
 import { getGptReplyAsHtml } from "./chat-gpt.js";
 
 export async function npcConversation(actorId, messagesArray) {
-    let actor = game.actors.get(actorId);
-    let background = actor.getFlag("it-has-brain", "background");
+	let actor = game.actors.get(actorId);
+	let background = actor.getFlag("it-has-brain", "background");
 
-    // If no background is found, generate it on the fly
-    if (!background) {
-        await npcBackgroundGenerator(actorId);
-        background = actor.getFlag("it-has-brain", "background");
-    }
-    let conversationHistory = actor.getFlag("it-has-brain", "journal") ?? [];
+	// If no background is found, generate it on the fly
+	if (!background) {
+		await npcBackgroundGenerator(actorId);
+		background = actor.getFlag("it-has-brain", "background");
+	}
+	let conversationHistory = actor.getFlag("it-has-brain", "journal") ?? [];
 
-    let prompt = `You are part of a table top RPG and are role-playing as ${actor.name}, who has the following background: ${background}. 
+	let prompt = `You are part of a table top RPG and are role-playing as ${actor.name}, who has the following background: ${background}. 
     In this role, you should remain in character at all times and your responses should reflect the decisions and conversations 
     ${actor.name} has had in the past.
 
     Here is some context from previous conversations:`;
 
-    for (let i = 0; i < conversationHistory.length; i++) {
-        prompt += ` \n${conversationHistory[i].character}: "${conversationHistory[i].message}"`;
-    }
+	for (let i = 0; i < conversationHistory.length; i++) {
+		prompt += ` \n${conversationHistory[i].character}: "${conversationHistory[i].message}"`;
+	}
 
-    prompt += ` \nNow, ${actor.name} has just received the following message from ${messagesArray[0].sender}: "${messagesArray[0].message}". 
+	prompt += ` \nNow, ${actor.name} has just received the following message from ${messagesArray[0].sender}: "${messagesArray[0].message}". 
   Given the context and ${actor.name}'s background, how would ${actor.name} respond?`;
 
 
-    var data = await getGptReplyAsHtml(prompt)
+	var data = await getGptReplyAsHtml(prompt)
 
-    // Update the actor's public notes (journal)
-    conversationHistory.push({
-        character: actor.name,
-        message: data
-    });
+	// Update the actor's public notes (journal)
+	conversationHistory.push({
+		character: actor.name,
+		message: data
+	});
 
-    // If conversation history exceeds certain length, trim it
-    if (conversationHistory.length > 50) { // choose a suitable length
-        conversationHistory = conversationHistory.slice(-50);
-    }
+	// If conversation history exceeds certain length, trim it
+	if (conversationHistory.length > 50) { // choose a suitable length
+		conversationHistory = conversationHistory.slice(-50);
+	}
 
-    await actor.setFlag("it-has-brain", "journal", conversationHistory);
+	await actor.setFlag("it-has-brain", "journal", conversationHistory);
 
-    game.socket.emit("module.it-has-brain.journalUpdated", { actorId: actor.id });
+	game.socket.emit("module.it-has-brain.journalUpdated", { actorId: actor.id });
 
-    await ChatMessage.create({
-        user: game.user.id,
-        speaker: {
-            actor: actor,
-            alias: actor.name
-        },
-        content: data,
-        sound: CONFIG.sounds.notification,
-    });
+	await ChatMessage.create({
+		user: game.user.id,
+		speaker: {
+			actor: actor,
+			alias: actor.name
+		},
+		content: data,
+		sound: CONFIG.sounds.notification,
+	});
 }
 
 
 export async function npcBackgroundGenerator(actorId) {
-    const actor = game.actors.get(actorId)
-    if (actor) {
-        console.log("For debugging, here is the actor model ", actor)
-        const generatedBackground = await getGptReplyAsHtml(`Generate a background story for this character name ${actor.name} and traits ${actor.data.system.traits.value.join(",")}`);
-        await actor.setFlag("it-has-brain", "background", generatedBackground);
-        game.socket.emit("module.it-has-brain.generatedBackground", { actorId, background: generatedBackground });
-    }
+	const actor = game.actors.get(actorId)
+	if (actor) {
+		console.log("For debugging, here is the actor model ", actor)
+		const generatedBackground = await getGptReplyAsHtml(`Generate a background story for this character name ${actor.name} and traits ${actor.data.system.traits.value.join(",")}`);
+		await actor.setFlag("it-has-brain", "background", generatedBackground);
+		game.socket.emit("module.it-has-brain.generatedBackground", { actorId, background: generatedBackground });
+	}
 }
